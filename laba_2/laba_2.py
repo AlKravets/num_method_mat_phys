7d80583a7e9a7a955a899215e8c2dbecfa78d040
Aleksey Kravets <alekravets976@gmail.com>
2019-11-10 23:40:07 +0200

    Done second laba and zvit

--- laba_2/laba_2.py
+++ laba_2/laba_2.py
@@ -57,21 +57,45 @@ h1, h2 = 0, 1
 H1, H2 = 1, 4
 
 def a(x):
-    # return np.sin(math.pi*x)
-    return 0
+    return np.sin(math.pi*x)
+    # return 0
 
 def f(x):
     return 2*x**2 + np.sin(2*x)
 
 
+## Изменим вид диф. Уравнения для работы 2 метода,для 1 метода форма не важна
+
+def mu(x):
+    # return np.exp(x + (4 * np.arctan((1-2*np.tan(math.pi*x/2))/3**0.5))/(3**0.5 * math.pi)\
+    #     +np.log(2-np.sin(math.pi *x)))
+    return np.exp(x + (4 * np.arctan((1-2*np.tan(math.pi*x/2))/3**0.5))/(3**0.5 * math.pi))
+
+def p_1 (x):
+    return -1*(np.sin(math.pi*x)-2)*mu(x)
+    # return p(x)
+    
+def p_1_for_integrate(x):
+    return 1/p_1(x)
+
+def q_1(x):
+    return q(x)*mu(x)
+    #  return q(x)
+
+def f_1(x):
+    return f(x)*mu(x)
+
+def a_1 (x):
+    return 0
+
 def right_function(x, i,j,N):
-    return p(x)*d_phi(x,i,N, 1/N)*d_phi(x,j,N,1/N) + a(x)*d_phi(x,i, N, 1/N)*phi(x,j, N, 1/N) + q(x)*phi(x,i,N, 1/N)*phi(x,j, N, 1/N)
+    return p_1(x)*d_phi(x,i,N, 1/N)*d_phi(x,j,N,1/N) + a_1(x)*d_phi(x,i, N, 1/N)*phi(x,j, N, 1/N) + q_1(x)*phi(x,i,N, 1/N)*phi(x,j, N, 1/N)
 def left_function(x,j,N):
-    return f(x)*phi(x,j, N, 1/N)
+    return f_1(x)*phi(x,j, N, 1/N)
 
 def A_ij (i,j,N):
-    res = integrate.quad(right_function,max((i-1)/N,0),min(1,(i+1)/N),args=(i,j, N),limit = 200)[0] + p(1)*phi(1,i, N, 1/N)*phi(1,j, N,1/N)*H1/H2\
-         + p(0)*phi(0,i, N, 1/N)*phi(0,j, N, 1/N)*h1/h2
+    res = integrate.quad(right_function,max((i-1)/N,0),min(1,(i+1)/N),args=(i,j, N),limit = 200)[0] + p_1(1)*phi(1,i, N, 1/N)*phi(1,j, N,1/N)*H1/H2\
+         + p_1(0)*phi(0,i, N, 1/N)*phi(0,j, N, 1/N)*h1/h2
     print(integrate.quad(right_function,max((i-1)/N,0),min(1,(i+1)/N),args=(i,j, N), limit = 100)[0],'  ', i, j)
     #print(p(1)*phi(1,i, N, 1/N)*phi(1,j, N,1/N)*H1/H2 + p(0)*phi(0,i, N, 1/N)*phi(0,j, N, 1/N)*h1/h2,'  ', i, j)
     return res
@@ -127,13 +151,20 @@ def test_2_plots(N1, N2):
     xx = np.linspace(0,1,100)
     y1 = create_data_for_plot(N1,xx)
     y2 = create_data_for_plot(N2,xx)
+    c1 = result_SLAR(N1)
+    c2 = result_SLAR(N2)
 
     fig,ax = plt.subplots()
-    ax.scatter(xx,y1, marker = 'X', s = 5)
-    ax.plot(xx,y2, color = 'red')
+    ax.scatter(xx,y1, marker = 'X', s = 5, label = '50 точек')
+    ax.plot(xx,y2, color = 'red', label = '100 точек')
     #ax.plot(xx,y1, color = 'blue')
-    #plt.legend("n1="+str(N1), "n2="+str(N2))
+    plt.legend()
     plt.show()
+    fig.savefig('photo3.jpg')
+
+    x1 = np.linspace(0,1,11)
+    for i in range(x1.shape[0]):
+        print('{0:.2f} & {1:.6f} \\'.format(x1[i], abs(y(x1[i],N1,c1)-y(x1[i],N2,c2))))
 
 
 
@@ -142,17 +173,6 @@ def test_2_plots(N1, N2):
 
 
 
-def p_1 (x):
-    # return -1*np.exp(x + (4 * np.arctan((1-2*np.tan(math.pi*x/2))/3**0.5))/(3**0.5 * math.pi)\
-    #     +np.log(2-np.sin(math.pi *x)))
-    return p(x)
-    
-def p_1_for_integrate(x):
-    return 1/p_1(x)
-
-def q_1(x):
-    # return q(x)/(np.sin(math.pi *x)-2)*-p_1(x)
-     return q(x)
 
 
 def _p_ (x_1,x_2,h):
@@ -162,7 +182,7 @@ def _q_ (x_1,x_2,h):
     return integrate.quad(q_1,x_1,x_2)[0]/h
 
 def _f_ (x_1,x_2,h):
-    return integrate.quad(f,x_1,x_2)[0]/h
+    return integrate.quad(f_1,x_1,x_2)[0]/h
 
 def _p_0_N (x_1,x_2,h):
     return 2*_p_ (x_1,x_2,h)
@@ -204,19 +224,54 @@ def create_SLAR_for_2_metod(x:np.ndarray):
     F[N] = h/2*_f_0_N(h*(N-0.5), h*N, h)
 
     u = np.linalg.solve(A,F)
+    #print(A)
     return u
 
 def test_metod_2(x):
     u = create_SLAR_for_2_metod(x)
-    #u = p_1(x)
-    print(u)
 
-    y1 = create_data_for_plot(x.shape[0],x)
+    y1 = laba_1.create_data_for_plot(10,x)
+
+    #y1 = create_data_for_plot(x.shape[0],x)
     fig,ax = plt.subplots()
-    ax.scatter(x,u, color = 'red')
-    ax.plot(x,y1, color = 'blue')
-    plt.legend('m_2', 'm_1')
+    ax.scatter(x,u, color = 'red', label = 'Интегро-интер. метод')
+    ax.plot(x,y1, color = 'blue', label = 'Метод Галёркина')
+    plt.legend()
     plt.show()
+    fig.savefig('photo2_1.jpg')
+
+def delta_3_metods(x):
+    y_0 = laba_1.create_data_for_plot(10, x)
+    y_1 = create_data_for_plot(x.shape[0],x)
+    y_2 = create_SLAR_for_2_metod(x)
+
+    for i in range(0,x.shape[0],10):
+        delta_0_1 = abs(y_0[i] - y_1[i])
+        delta_0_2 = abs(y_0[i] - y_2[i])
+        delta_1_2 = abs(y_2[i] - y_1[i])
+        print('{0:.2f} & {1:.6f} & {2:.6f} & {3:.6f} & {4:.6f} & {5:.6f} & {6:.6f}'.format(x[i], y_0[i], y_1[i], y_2[i], delta_0_1, delta_0_2, delta_1_2))
+
+
+
+def test_2_plots_2_metods():
+    x1 = np.linspace(0,1,51)
+    x2 = np.linspace(0,1,101)
+    y1 = create_SLAR_for_2_metod(x1)
+    y2 = create_SLAR_for_2_metod(x2)
+    
+
+    fig,ax = plt.subplots()
+    ax.scatter(x1,y1, marker = 'X', s = 5, label = '50 точек')
+    ax.plot(x2,y2, color = 'red', label = '100 точек')
+    #ax.plot(xx,y1, color = 'blue')
+    plt.legend()
+    plt.show()
+    fig.savefig('photo3_1.jpg')
+
+    x1 = np.linspace(0,1,11)
+    for i in range(x1.shape[0]):
+        print('{0:.2f} & {1:.6f} \\'.format(x1[i], abs(y1[i*5] - y2[i*10])))
+
 
 
 
@@ -234,8 +289,12 @@ if __name__ == '__main__':
 
     #test_2_plots(50,100)
 
-    x = np.linspace(0,1,50)
-    test_metod_2(x)
-    print(p_1(0.3))
+    x = np.linspace(0,1,101)
+    #test_metod_2(x)
+    #print(p_1(0.3))
+    delta_3_metods(x)
+
+    #test_2_plots_2_metods()
+
 
 
